4c010d9a39bb83b03f7c64b8a14106ffe2a59140
Marlene Guzman <guzmanmarlene@yahoo.com>
2022-06-23 22:52:31 -0700

    start game function and event listener are firing

--- js/app.js
+++ js/app.js
@@ -17,7 +17,15 @@
 //   const game = new Game(); 
 // game.getRandomPhrase().addPhraseToDisplay();
 
-const game = new Game(); 
-game.startGame(); 
-console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
+// const game = new Game(); 
+// game.startGame(); 
+// console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
 
+let game;
+const startButton = document.querySelector('#btn__reset')
+console.log(startButton)
+startButton.addEventListener('click', (e)=>{
+  //Inside the callback function for this click event listener, use your `game` variable to instantiate a new Game object. Call the `startGame()` method on this new Game object.
+  game = new Game()
+  game.startGame()
+})
